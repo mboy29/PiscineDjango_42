7cef30e430a93dda0e1656326caa93d091e4cdd4
Mel <melody@42nice.fr>
2024-08-10 09:45:13 +0200

    3-Sessions Little fixes OK

--- 3-Sessions/ex/middleware.py
+++ 3-Sessions/ex/middleware.py
@@ -3,10 +3,46 @@ from django.shortcuts import redirect
 from django.urls import reverse
 
 class MiddlewareRedirectAuthenticatedUser:
+
+    """
+    Middleware class to redirect authenticated users to 
+    the home page.
+
+    Attributes:
+        get_response (function): The next middleware function.
+
+    Methods:
+        __init__(get_response): The constructor
+        __call__(request): The middleware function.
+    """
+
     def __init__(self, get_response):
+
+        """
+        The constructor for the MiddlewareRedirectAuthenticatedUser class.
+        
+        Args:
+            get_response (function): The next middleware function.
+
+        Returns:
+            None
+        """
+
         self.get_response = get_response
 
     def __call__(self, request):
+
+        """ 
+        The middleware function to redirect authenticated users to 
+            the home page.
+        
+        Args:
+            request (HttpRequest): The request object.
+        
+        Returns:
+            HttpResponse: The HTTP response object.
+        """
+
         restricted_paths = [
             reverse('ex:login'),
             reverse('ex:register')
